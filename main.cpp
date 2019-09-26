c1efb6e8ee67cdf95051a8e35fd4f3c90f77256c
Mikhail Abramov <beavisabra@yandex.ru>
2019-09-26 21:50:39 +0400

    Переименования

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main()
     txCreateWindow(1000,800);
 
 
-    MapObject a[4];
-    a[0] = { 810,   0, 930, 200, txLoadImage ("Album/House/house1.bmp"), ""};
-    a[1] = { 810, 300, 930, 500, txLoadImage ("Album/House/house2.bmp"), ""};
-    a[2] = { 810, 600, 930, 800, txLoadImage ("Album/House/house3.bmp"), ""};
-    a[3] = { 810, 200, 930, 300, txLoadImage ("Album/House/House4.bmp"), ""};
+    MapObject houseVariants[4];
+    houseVariants[0] = { 810,   0, 930, 200, txLoadImage ("Album/House/house1.bmp"), ""};
+    houseVariants[1] = { 810, 300, 930, 500, txLoadImage ("Album/House/house2.bmp"), ""};
+    houseVariants[2] = { 810, 600, 930, 800, txLoadImage ("Album/House/house3.bmp"), ""};
+    houseVariants[3] = { 810, 200, 930, 300, txLoadImage ("Album/House/House4.bmp"), ""};
 
-    MapObject b1 = { 100, 100, 600, 600, txLoadImage ("Album/House/house2.bmp"), ""};
-    MapObject d =  { 600, 550, 900, 700, txLoadImage ("Album/Animals/DoG.bmp"), ""};
+    MapObject2 b1 = { 100, 100, 600, 600, txLoadImage ("Album/House/house2.bmp"), ""};
+    MapObject2 d =  { 600, 550, 900, 700, txLoadImage ("Album/Animals/DoG.bmp"), ""};
 
-    Button b[7];
-    b[0] = {0,0, "ƒŒÃ"};
-    b[1] = {100,0, "—Œ¡¿ ¿"};
-    b[2] = {200,0, "œ≈“”’"};
-    b[3] = {300,0, "’–ﬁÿ ¿"};
-    b[4] = {400,0, "ƒﬂƒﬂ\nœ≈“ﬂ"};
-    b[5] = {500,0, "—“¿—"};
-    b[6] = {600,0, "Ã¿–»Õ¿"};
+    Button buttons[7];
+    buttons[0] = {0,0, "ƒŒÃ"};
+    buttons[1] = {100,0, "—Œ¡¿ ¿"};
+    buttons[2] = {200,0, "œ≈“”’"};
+    buttons[3] = {300,0, "’–ﬁÿ ¿"};
+    buttons[4] = {400,0, "ƒﬂƒﬂ\nœ≈“ﬂ"};
+    buttons[5] = {500,0, "—“¿—"};
+    buttons[6] = {600,0, "Ã¿–»Õ¿"};
 
     while(!GetAsyncKeyState(VK_ESCAPE))
     {
@@ -36,13 +36,13 @@ int main()
 
         for (int nomer_kartinki = 0; nomer_kartinki <= 3 ; nomer_kartinki ++)
         {
-            a[nomer_kartinki].drawMapObject();
+            houseVariants[nomer_kartinki].drawMapObject();
         }
 
-        for (int nomer = 0; nomer < 7; nomer++)
+        for (int nomer_knopki = 0; nomer_knopki < 7; nomer_knopki++)
         {
-            b[nomer].drawButton();
-            if (b[nomer].Click ())
+            buttons[nomer_knopki].drawButton();
+            if (buttons[nomer_knopki].Click ())
             {
                 txTextOut(100, 500, "¬˚ Ì‡Ê‡ÎË Ì‡ ÍÌÓÔÍÛ");
             }
@@ -60,9 +60,9 @@ int main()
     }
 
 
-    txDeleteDC(a[0].picture);
-    txDeleteDC(a[1].picture);
-    txDeleteDC(a[2].picture);
+    txDeleteDC(houseVariants[0].picture);
+    txDeleteDC(houseVariants[1].picture);
+    txDeleteDC(houseVariants[2].picture);
 
     return 0;
 }
--- mapoject.cpp
+++ mapoject.cpp
@@ -11,6 +11,22 @@ struct MapObject
 
     void drawMapObject()
     {
+        Win32::TransparentBlt(txDC(), x,  y, x2 - x, y2 - y, picture, 0, 0, 500, 350, TX_WHITE);
+        txDrawText(      x,  y, x2, y2, text);
+    }
+};
+
+struct MapObject2
+{
+    int x;
+    int y;
+    int x2;
+    int y2;
+    HDC picture;
+    const char* text;
+
+    void drawMapObject()
+    {
         txBitBlt(txDC(), x,  y, x2 - x, y2 - y, picture);
         txDrawText(      x,  y, x2, y2, text);
     }
