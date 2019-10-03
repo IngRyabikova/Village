b6b7d78c083f7c032c32219b9d54d79a29ad90d1
IvanKorneychuk <55238727+IvanKorneychuk@users.noreply.github.com>
2019-10-03 16:30:52 +0300

    по убирал белый фон с домов

--- mapoject.cpp
+++ mapoject.cpp
@@ -8,10 +8,12 @@ struct MapObject
     int y2;
     HDC picture;
     const char* text;
+    int width;
+    int hight;
 
     void drawMapObject()
     {
-        Win32::TransparentBlt(txDC(), x,  y, x2 - x, y2 - y, picture, 0, 0, 500, 350, TX_WHITE);
+        Win32::TransparentBlt(txDC(), x,  y, x2 - x, y2 - y, picture, 0, 0, width, hight, TX_WHITE);
         txDrawText(      x,  y, x2, y2, text);
     }
 };
